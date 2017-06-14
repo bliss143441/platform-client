94aa90add9ef92c8f364866afc540ff5e68a96dd
bliss143441 <bliss143441@yahoo.com>
2017-06-14 16:05:06 +0800

    npm updates

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 // Configure your Ushahidi deployment
 //
 // Uncomment lines below to configure your deployment
-// window.ushahidi = {
-// 	backendUrl : "https://ushahidi-platform-api-release.herokuapp.com",
-// 	mapboxApiKey: ""
-// };
+window.ushahidi = {
+ 	backendUrl : 'http://localhost/platform/httpdocs/',
+ 	//mapboxApiKey: ''
+};
